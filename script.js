21acdbca486355381836b8a7d00e608c91ead187
gabrielcoveiro <148216635+GabrielSumb@users.noreply.github.com>
2024-08-30 16:47:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.querySelector('.menu-hamburguer').addEventListener('click', function() 
     document.querySelector('.menu-cascata').classList.toggle('show');
 });
 
-
-
-
+document.querySelector('.menu-sucos').addEventListener('click', function() {
+    this.classList.toggle('show-sucos');
+});
 
 
