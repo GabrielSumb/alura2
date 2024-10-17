8b7f2b04929b59a2d5d227f4bdf9eda5c89fefa8
gabrielcoveiro <148216635+GabrielSumb@users.noreply.github.com>
2024-10-17 12:02:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
   const checkbox = document.getElementById('checkbox');
-  
+
   checkbox.addEventListener('change', function() {
-    if(checkbox.checked) {
+    if (checkbox.checked) {
       document.body.classList.add('dark-mode');
     } else {
       document.body.classList.remove('dark-mode');
