a7b584b98e841626d51cce2541057dcb0656aab6
gabrielcoveiro <148216635+GabrielSumb@users.noreply.github.com>
2024-10-17 12:06:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const checkbox = document.getElementById('checkbox');
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
 
-  checkbox.addEventListener('change', function() {
-    if (checkbox.checked) {
-      document.body.classList.add('dark-mode');
-    } else {
-      document.body.classList.remove('dark-mode');
-    }
-  });
+document.querySelector('.next').addEventListener('click', () => {
+    changeSlide(1);
 });
+
+document.querySelector('.prev').addEventListener('click', () => {
+    changeSlide(-1);
+});
+
+function changeSlide(direction) {
+    currentSlide = (currentSlide + direction + slides.length) % slides.length;
+    slides.forEach((slide, index) => {
+        slide.style.transform = translateX(${100 * (index - currentSlide)}%);
+    });
+}
