c8eee241e64e38ad6ea43c521253aea4a2d4889b
gabrielcoveiro <148216635+GabrielSumb@users.noreply.github.com>
2024-10-17 11:45:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("Script loaded!");
+  const checkbox = document.getElementById('checkbox');
+  
+  checkbox.addEventListener('change', function() {
+    if(checkbox.checked) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  });
 });
-
