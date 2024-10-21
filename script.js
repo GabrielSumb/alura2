4bbf6e9a6938ce593147a1e15645b841d8bc0119
gabrielcoveiro <148216635+GabrielSumb@users.noreply.github.com>
2024-10-20 21:48:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const body = document.body;
     const menuButton = document.querySelector('.menu-button');
     const menu = document.querySelector('.menu');
-    const submenuItems = document.querySelectorAll('.menu > ul > li');
     const temaBoxes = document.querySelectorAll('.tema-box');
     const backToTopButton = document.getElementById('back-to-top');
     let currentTheme = 'dark';
 
     // Alternar tema
     themeToggle.addEventListener('click', function() {
-        if (currentTheme === 'dark') {body.classList.add('light-theme');
-            temaBoxes.forEach(box => box.classList.add('light-theme'));
+        if (currentTheme === 'dark') {
+            body.classList.add('light-theme');
+            temaBoxes.forEach(box => box.classList.add('light-theme')); // Adiciona classe de tema claro a todos os quadrados
             currentTheme = 'light';
-            themeToggle.textContent = '🌙'; // Mudar para o ícone de lua
+            themeToggle.textContent = '🌙'; // Muda para símbolo da lua
         } else {
             body.classList.remove('light-theme');
-            temaBoxes.forEach(box => box.classList.remove('light-theme'));
+            temaBoxes.forEach(box => box.classList.remove('light-theme')); // Remove classe de tema claro de todos os quadrados
             currentTheme = 'dark';
-            themeToggle.textContent = '☀️'; // Mudar para o ícone de sol
+            themeToggle.textContent = '☀️'; // Muda para símbolo do sol
         }
     });
 
@@ -28,23 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
         menuButton.classList.toggle('active');
     });
 
-    // Exibir submenu
-    submenuItems.forEach(item => {
-        item.addEventListener('mouseenter', function() {
-            const submenu = item.querySelector('.submenu');
-            if (submenu) {
-                submenu.style.display = 'block';
-            }
-        });
-        item.addEventListener('mouseleave', function() {
-            const submenu = item.querySelector('.submenu');
-            if (submenu) {
-                submenu.style.display = 'none';
-            }
-        });
-    });
-
-    // Mostrar botão voltar ao topo
+    // Botão Voltar ao Topo
     window.addEventListener('scroll', function() {
         if (window.scrollY > 300) {
             backToTopButton.style.display = 'block';
@@ -53,7 +37,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Voltar ao topo
     backToTopButton.addEventListener('click', function() {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     });
